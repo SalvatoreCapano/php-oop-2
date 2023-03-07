9c572adbcfb7129d66358d31bbe959747126ad30
Salvatore Capano <capano_salvatore@libero.it>
2023-03-07 21:40:44 +0100

    Agg. Exception per il prezzo

--- Models/Product.php
+++ Models/Product.php
@@ -7,8 +7,19 @@ class Product {
     public $imgUrl = 'https://picsum.photos/200/300';   //Per semplicita' uso immagini placeholder
 
     function __construct(string $_name, int $_price, int $_quantity, Category $_category) {
+        
+        if ($_price <= 0) {
+                throw new Exception('Price must be equal or greater than zero!');
+        }
+
+        try {
+            $this->price = $_price;
+        } 
+        catch (Exception $e) {
+            echo 'Errore: ' . $e->getMessage();
+        }
+        
         $this->name = $_name;
-        $this->price = $_price;
         $this->quantity = $_quantity;
         $this->category = $_category;
     }
